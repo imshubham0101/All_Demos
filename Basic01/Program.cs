e54740ec7171b1dd35e3e7052b7e624bce15beed
C <C@C>
2019-12-09 09:43:36 +0530

    Add project files.

---
+++ Basic01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic01
+{
+
+    public delegate void Mydelegate(String msg);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydelegate pointer = new Mydelegate(SayHello);
+
+            Student s1 = new Student();
+
+            s1.Admitted += pointer;
+
+            s1.DoAdmit("Shubham");
+
+            Console.ReadLine();
+
+
+        }
+
+      
+
+        private static void SayHello(string name)
+        {
+            Console.WriteLine("Welcome Mr/Mrs "+name);
+        }
+    }
+
+    public class Student
+    {
+        public event Mydelegate Admitted ; 
+
+        public void DoAdmit(String name)
+        {
+            Admitted(name);
+        }
+    }
+}
